52cb608bd5864a114558ba83b1c4e6ab453fb729
Douglas Mendes <capudougmor@gmail.com>
2020-07-08 11:19:32 -0300

    criada pagina registro

---
+++ src/pages/Registro/index.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+
+import './styles.css'
+import api from '../../services/api'
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const Registro = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  async function handleRegister(e) {
+    e.preventDefault()
+
+    const data = {
+      name, email,
+    }
+
+    try {
+      const response = await api.post('company', data)
+
+      alert(`Seu id de acesso: ${response.data.id}`)
+    } catch (err) {
+      alert('Erro no cadastro, tente novamente.', err)
+    }
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="registro">
+        <h1>Dados para o registro</h1>
+          
+          <form onSubmit={handleRegister}>
+            <div className='fields'>
+              <label>Empresa:</label>
+              <input 
+                value={name}
+                onChange={e => setName(e.target.value)}
+                type="text"
+                placeholder='Nome da empresa'
+              />
+            </div>
+            <div className='fields'>
+              <label>E-mail:</label>
+              <input
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                type="text"
+                placeholder='E-mail'
+              />
+            </div>
+            <button className='button' type='submit' >Registrar empresa</button>
+          </form>
+        </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Registro
--- src/routes.js
+++ src/routes.js
@@ -4,18 +4,18 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Pesquisa from './pages/Pesquisa';
-// import NewIncident from './pages/NewIncident'
+import Registro from './pages/Registro'
 
-export default function Routes() {// rotas tambem sao componentes por isso sao exportadas
+export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>    
                 <Route path="/" exact component={Home} /> 
                 <Route path="/sobre" component={Sobre} />
                 <Route path="/pesquisa" component={Pesquisa} />
-                {/* <Route path="/incident/new" component={NewIncident} /> */}
+                <Route path="/registro" component={Registro} />
 
             </Switch>        
         </BrowserRouter>
-    )// o exact é para que o caminho seja exatamento este
+    )
 }
